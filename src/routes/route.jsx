cfb6667821fce1ac5e7f778c721cad0feb33008c
unknown <moiz1rehman@gmail.com>
2024-09-23 12:56:54 +0500

    deployment issues

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -6,11 +6,12 @@ import Login from '../pages/login';
 import TaskList from '../pages/taskList';
 import SignUp from '../pages/signUp';
 import Home from '../pages/home';
-import Header from '../component/navbar';
+import Navbar from '../component/navbar';
+//import Header from '../component/navbar';
 const RouteConfig = () => {
   return (
     <>
-      <Header /> {/* No need for Router here */}
+      <Navbar/> {/* No need for Router here */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
